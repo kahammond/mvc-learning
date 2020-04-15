e6f77517deaee8fbe7ff7eeafddadb29e8f64054
Keith Hammond <34716170+kahammond@users.noreply.github.com>
2020-04-15 14:51:34 -0400

    Create WeatherEditorModel.cs

---
+++ weather widget/Models/InlineEditors/WeatherEditor/WeatherEditorModel.cs
@@ -0,0 +1,9 @@
+namespace DancingGoat.Models.InlineEditors.WeatherEditor
+{
+    public class WeatherEditorModel
+    {
+        public string PropertyName { get; set; }
+
+        public string DegreeUnit { get; set; }
+    }
+}
