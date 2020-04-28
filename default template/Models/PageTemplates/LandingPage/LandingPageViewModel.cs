c30d2972514c1254794374a8487b8547326cafb5
Keith Hammond <34716170+kahammond@users.noreply.github.com>
2020-04-28 11:27:54 -0400

    Create LandingPageViewModel.cs

---
+++ default template/Models/PageTemplates/LandingPage/LandingPageViewModel.cs
@@ -0,0 +1,8 @@
+namespace BlankSite.Models.PageTemplates.LandingPage
+{
+    public class LandingPageViewModel
+    {
+        public bool ShowTitle { get; set; }
+        public string DocumentName { get; set; }
+    }
+}
