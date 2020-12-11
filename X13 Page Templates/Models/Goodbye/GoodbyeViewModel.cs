655bbf056fcdc98b03501c6043cd20d891cbff8e
Keith Hammond <34716170+kahammond@users.noreply.github.com>
2020-12-11 14:50:37 -0500

    Create GoodbyeViewModel.cs

---
+++ X13 Page Templates/Models/Goodbye/GoodbyeViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewSite.Models
+{
+    public class GoodbyeViewModel
+    {
+        public bool ShowTitle { get; set; }
+        public string DocumentCulture { get; set; }
+        public string DocumentName { get; set; }
+    }
+}
