ef87cf202c4ec063174c2022ebbb51d53df57f69
Keith Hammond <34716170+kahammond@users.noreply.github.com>
2020-12-11 14:49:19 -0500

    Create HomeController.cs

---
+++ X13 Page Templates/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using Kentico.PageBuilder.Web.Mvc.PageTemplates;
+using Microsoft.AspNetCore.Mvc;
+using NewSite.PageTemplates;
+
+[assembly: RegisterPageTemplate("Keith.Hello", "Hello page", typeof(GoodbyePageProperties), customViewName: "~/Views/Shared/PageTemplates/_Hello.cshtml",
+        Description = "Hello.", IconClass = "icon-l-text")]
+
+namespace NewSite.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index(int documentId)
+        {
+            // Returns a TemplateResult object, created with an identifier of the page as its parameter
+            // Automatically initializes the page builder feature for any editable areas placed within templates
+            return new TemplateResult(documentId);
+        }
+    }
+}
