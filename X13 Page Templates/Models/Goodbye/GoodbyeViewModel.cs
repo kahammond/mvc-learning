70f1aff8611b0f2595c1b6d9dc6b4e485141b8f8
Keith Hammond <34716170+kahammond@users.noreply.github.com>
2021-05-04 11:19:35 -0400

    Delete X13 Page Templates directory

--- X13 Page Templates/Models/Goodbye/GoodbyeViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NewSite.Models
-{
-    public class GoodbyeViewModel
-    {
-        public bool ShowTitle { get; set; }
-        public string DocumentCulture { get; set; }
-        public string DocumentName { get; set; }
-    }
-}
